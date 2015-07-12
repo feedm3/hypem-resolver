b344bf05ed8b0a9f83d10b69cd428776fc34f13d
Fabian Dietenberger <piano.fabian@t-online.de>
2015-07-12 11:41:07 +0200

    Add test for the result function

--- test/hypem-resolver-test.js
+++ test/hypem-resolver-test.js
@@ -1,3 +1,14 @@
-/**
- * Created by Fabian on 12.07.2015.
- */
+// Copyright 2015 Fabian Dietenberger
+
+var should = require('chai').should(),
+    hypemResolver = require('../hypem-resolver');
+
+describe('Use the hypem link to get the songs url', function () {
+    describe('if the song is hosted on soundcloud', function () {
+        it('should return a soundcloud url', function (done) {
+            var soundcloudUrl = hypemResolver.getUrl('adad');
+            soundcloudUrl.should.be.a('string');
+            done();
+        })
+    });
+});

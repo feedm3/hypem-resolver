1149f3972ba600a783a37cd58bfba30300194295
Fabian Dietenberger <piano.fabian@t-online.de>
2015-07-12 18:33:40 +0200

    Add async methods to get the URL

--- hypem-resolver.js
+++ hypem-resolver.js
@@ -1,6 +1,6 @@
 //  Copyright 2015 Fabian Dietenberger
 
-'use-strict'
+'use-strict';
 
 var q = require('q'),
     request = require('request');
@@ -8,12 +8,24 @@ var q = require('q'),
 var hypemResolver = {};
 
 hypemResolver.getByUrl = function (hypemUrl) {
-    var resultUrl = "";
+    var resultUrl = "https://soundcloud.com/griz/summer-97-ft-muzzy-bearr";
     return resultUrl;
+};
+
+hypemResolver.getByUrlAsync = function (hypemUrl, callback) {
+    process.nextTick(function () {
+        callback(hypemResolver.getByUrl(hypemUrl));
+    });
 };
 
 hypemResolver.getById = function (hypemId) {
     return this.getByUrl("http://hypem.com/track/" + hypemId);
+};
+
+hypemResolver.getByIdAsync = function (hypemId, callback) {
+    process.nextTick(function () {
+        callback(hypemResolver.getById(hypemId));
+    });
 };
 
 module.exports = hypemResolver;

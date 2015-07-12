57e6823c22abe2267547e99669c120379865cb4d
Fabian Dietenberger <piano.fabian@t-online.de>
2015-07-12 11:40:55 +0200

    Add resolve function body

--- hypem-resolver.js
+++ hypem-resolver.js
@@ -1,3 +1,15 @@
-/**
- * Created by Fabian on 12.07.2015.
- */
+//  Copyright 2015 Fabian Dietenberger
+
+'use-strict'
+
+var q = require('q'),
+    request = require('request');
+
+var hypemResolver = {};
+
+hypemResolver.getUrl = function (hypemUrl) {
+    var resultUrl = "";
+    return resultUrl;
+};
+
+module.exports = hypemResolver;
